5454e062ac6eff2917fe802189127a22098f9c81
madhank_tratum <madhank@tratumtech.com>
2020-07-01 11:16:12 +0530

    Add: Main activity added

--- app/src/main/java/com/assignment/view/MainActivity.kt
+++ app/src/main/java/com/assignment/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.assignment
+package com.assignment.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.assignment.R
 
 class MainActivity : AppCompatActivity() {
 
