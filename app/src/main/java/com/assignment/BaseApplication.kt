74efe2a78e8bd9882f4b87bde0f4688b02ab1158
madhank_tratum <madhank@tratumtech.com>
2020-07-01 21:36:01 +0530

    Feat: Dependency injection builder created

--- app/src/main/java/com/assignment/BaseApplication.kt
+++ app/src/main/java/com/assignment/BaseApplication.kt
@@ -1,4 +1,11 @@
 package com.assignment
 
-class BaseApplication {
+import com.assignment.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+class BaseApplication: DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().application(this).build()
+    }
 }
